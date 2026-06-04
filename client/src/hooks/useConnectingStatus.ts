bf55b5130577fba210f3c3d493e440b6851630f6
“Mizan” <macbook@macs-MacBook-Pro.local>
2026-06-04 21:17:52 +0200

    fix v5

--- client/src/hooks/useConnectingStatus.ts
+++ client/src/hooks/useConnectingStatus.ts
@@ -6,26 +6,32 @@ export default function useSocketStatus() {
 
   useEffect(() => {
     socket.on("connect", () => {
+      console.log("Connected to WebSocket server");
       setStatus("connected");
     });
 
     socket.on("disconnect", () => {
+      console.log("Disconnected from WebSocket server");
       setStatus("disconnected");
     });
 
     socket.io.on("reconnect_attempt", () => {
+      console.log("Attempting to reconnect to WebSocket server");
       setStatus("reconnecting");
     });
 
     socket.io.on("reconnect", () => {
+      console.log("Reconnected to WebSocket server");
       setStatus("reconnect");
     });
 
     socket.on("connect_error", () => {
+      console.log("Error occurred while connecting to WebSocket server");
       setStatus("connect_error");
     });
 
     socket.on("auth_error", () => {
+      console.log("Authentication error occurred");
       setStatus("auth_error");
     });
 
