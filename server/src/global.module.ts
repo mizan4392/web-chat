4efd6224e7cfabc4557651299319318b21577521
“Mizan” <macbook@macs-MacBook-Pro.local>
2026-07-22 20:37:41 +0200

    fix

--- server/src/global.module.ts
+++ server/src/global.module.ts
@@ -1,6 +1,6 @@
 import { CloudinaryModule } from './../cloudinary.module';
 import { Global, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { AuthGuard } from './AuthGard';
 import { JwtService } from '@nestjs/jwt';
@@ -8,31 +8,21 @@ import { GlobalService } from './global.service';
 
 import { WebSocketGuard } from './WebSocketGard';
 
+const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || '9001', 10),
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'postgres',
+  database: process.env.DATABASE_NAME || 'web_chat',
+  autoLoadEntities: true,
+  synchronize: true,
+};
+console.log('db config', dbConfig);
+
 @Global()
 @Module({
-  imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   url: DB_URL,
-    //   ssl: {
-    //     rejectUnauthorized: false, // Allows self-signed certificates (sufficient for Aiven)
-    //   },
-    //   autoLoadEntities: true,
-    //   synchronize: true,
-    //   entities: [User, Group, GroupMember, GroupMessage],
-    // }),
-    TypeOrmModule.forRoot({
-      type: (process.env.DATABASE_TYPE as any) || 'postgres',
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT) || 9001,
-      username: process.env.DATABASE_USER || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'postgres',
-      database: process.env.DATABASE_NAME || 'web_chat',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    CloudinaryModule,
-  ],
+  imports: [TypeOrmModule.forRoot({ ...dbConfig }), CloudinaryModule],
   providers: [AuthGuard, JwtService, GlobalService, WebSocketGuard],
   exports: [AuthGuard, JwtService, GlobalService, WebSocketGuard],
 })
