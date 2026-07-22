0a081374913883cbb9e0b215d2c667715c25b8b0
“Mizan” <macbook@macs-MacBook-Pro.local>
2026-07-22 20:16:46 +0200

    dot env setup

--- server/src/global.module.ts
+++ server/src/global.module.ts
@@ -24,7 +24,7 @@ import { WebSocketGuard } from './WebSocketGard';
     TypeOrmModule.forRoot({
       type: (process.env.DATABASE_TYPE as any) || 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
-      port: parseInt(process.env.DATABASE_PORT) || 5432,
+      port: parseInt(process.env.DATABASE_PORT) || 9001,
       username: process.env.DATABASE_USER || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'postgres',
       database: process.env.DATABASE_NAME || 'web_chat',
